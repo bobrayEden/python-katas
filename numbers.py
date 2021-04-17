b0651d75f7aad5506f75d179f5e660df28113607
bobrayEden <o.nocaudie@googlemail.com>
2021-04-17 11:34:10 +0200

    Make readable kata in numbers

--- numbers.py
+++ numbers.py
@@ -33,3 +33,28 @@ def len_curve(n):
 from math import sqrt
 def len_curve(n):
     return round(sum(sqrt((2*k+1)**2/n/n + 1) for k in range(n))/n, 9)
+
+"""
+Make Readable
+Format seconds as int to string as hh:mm:ss
+"""
+# Ma solution
+def make_readable(seconds):
+    hours, mod = divmod(seconds, 3600)
+    min, sec = divmod(mod, 60)
+    times = [hours, min, sec]
+    res = ''
+    for num, x in enumerate(times, start = 1):
+        if x < 10:
+            res += '0' + str(x)
+        else:
+            res += str(x)
+        if num < 3:
+            res += ':'
+    return res
+
+# Solution upvote
+def make_readable(seconds):
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    return "{:02}:{:02}:{:02}".format(hours, minutes, seconds)
