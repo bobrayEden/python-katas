44ca417e21642c2dfe876db32fb16f42191d3b07
bobrayEden <o.nocaudie@googlemail.com>
2021-04-22 08:17:31 -0400

    kata pig latin

--- strings.py
+++ strings.py
@@ -22,3 +22,29 @@ def anagrams(word, words):
 
 # Solution pythonesque
 def anagrams(word, words): return [item for item in words if sorted(item)==sorted(word)]
+
+"""
+Stop gninnipS my sdrow
+Retourner la string originale en inversant l'ordre des lettres des mots en contenant 5 ou plus
+"""
+# Ma solution
+def spin_words(sentence):
+    words = sentence.split()
+    for i, x in enumerate(words):
+        if len(x) > 4:
+            words[i] = "".join(reversed(list(x)))
+    s = ' '
+    return s.join(words)
+
+# Solution upvote
+# Avec une grosse list comprehension
+def spin_words(sentence):
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+
+"""
+Pig it Latin
+Retour : Prendre la première lettre de chaque mot et la passer à la fin, laisser la ponctuation telle quelle
+"""
+# Ma solution : Le list(text.split()) n'est pas nécessaire car split() renvoie une liste
+def pig_it(text):
+    return " ".join(x[1:] + x[0] + "ay" if x.isalpha() else x for x in list(text.split()))
